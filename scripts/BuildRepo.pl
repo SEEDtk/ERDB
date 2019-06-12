0f771e299313f7616cd66e0b854ed27474bff61f
Bruce Parrello <bparrello@figresearch.com>
2019-06-11 21:34:01 -0500

    Time stamps on errors.

--- scripts/BuildRepo.pl
+++ scripts/BuildRepo.pl
@@ -177,227 +177,232 @@ my $opt = ScriptUtils::Opts('',
 # Get a helper object and the associated statistics object.
 my $loader = CopyFromSeed->new($opt);
 my $stats = $loader->stats;
-# Create the protein family repo.
-my $protFamRepo = ProtFamRepo->new($stats);
-# If we are NOT resuming, get the chem data.
-if (! $opt->resume) {
-    # Get the chemistry data.
-    print "Refreshing chemistry data.\n";
-    Shrub::ChemLoader::RefreshFiles($opt->repo);
-}
-# Connect to the standard input.
-my $ih = ScriptUtils::IH($opt->input);
-# Are we clearing?
-if ($opt->clear) {
-    print "Erasing genome repository.\n";
-    my $genomeDir = $loader->genome_repo();
-    File::Copy::Recursive::pathempty($genomeDir) ||
-        die "Error clearing $genomeDir: $!";
-    print "Erasing subsystem repository.\n";
-    my $subsysDir = $loader->subsys_repo();
-    File::Copy::Recursive::pathempty($subsysDir) ||
-        die "Error clearing $subsysDir: $!";
-    my $sampleDir = $loader->sampleRepo;
-    if (-d $sampleDir) {
-        print "Erasing sample repository.\n";
-        File::Copy::Recursive::pathempty($sampleDir) ||
-            die "Error clearing $sampleDir: $!";
+# We want time stamps and stats when an error occurs.
+eval {
+    # Create the protein family repo.
+    my $protFamRepo = ProtFamRepo->new($stats);
+    # If we are NOT resuming, get the chem data.
+    if (! $opt->resume) {
+        # Get the chemistry data.
+        print "Refreshing chemistry data.\n";
+        Shrub::ChemLoader::RefreshFiles($opt->repo);
     }
-}
-# This hash is used to prevent us from reloading genomes we've already processed.
-my %genomesProcessed;
-if ($opt->resume) {
-    # We are resuming.  Denote that we've processed any genome that has a nonempty
-    # "non-peg-info" file. To do this, we need to do a recursive search through
-    # the GenomeData directory.
-    print "Scanning for existing genomes.\n";
-    my $genomeDir = $loader->genome_repo();
-    my $genomeHash = $loader->FindGenomeList($genomeDir, nameless => 1);
-    # Loop through the genomes in the directory, checking for "non-peg-info".
-    my $count = 0;
-    for my $genome (keys %$genomeHash) {
-        my $dirName = $genomeHash->{$genome};
-        if (-s "$dirName/non-peg-info") {
-            $genomesProcessed{$genome} = 1;
-            $stats->Add("genome-alreadyLoaded" => 1);
-            $count++;
+    # Connect to the standard input.
+    my $ih = ScriptUtils::IH($opt->input);
+    # Are we clearing?
+    if ($opt->clear) {
+        print "Erasing genome repository.\n";
+        my $genomeDir = $loader->genome_repo();
+        File::Copy::Recursive::pathempty($genomeDir) ||
+            die "Error clearing $genomeDir: $!";
+        print "Erasing subsystem repository.\n";
+        my $subsysDir = $loader->subsys_repo();
+        File::Copy::Recursive::pathempty($subsysDir) ||
+            die "Error clearing $subsysDir: $!";
+        my $sampleDir = $loader->sampleRepo;
+        if (-d $sampleDir) {
+            print "Erasing sample repository.\n";
+            File::Copy::Recursive::pathempty($sampleDir) ||
+                die "Error clearing $sampleDir: $!";
         }
     }
-    print "$count pre-existing genomes found.\n";
-}
-# Get the first line.
-my $line = <$ih>;
-# Loop through the input file.
-while (defined $line) {
-    $line =~ s/[\r\n]+$//;
-    $stats->Add(commandLines => 1);
-    my ($command, @parms) = split /\t/, $line;
-    # Determine the purpose of this line.
-    if ($command eq '+SEED') {
-        # Here we have a new SEED.
-        my ($figDisk, $priv) = @parms;
-        # Insure we have a FIG disk.
-        if (! $figDisk) {
-            die "No FIG disk specified on +SEED command line.\n";
+    # This hash is used to prevent us from reloading genomes we've already processed.
+    my %genomesProcessed;
+    if ($opt->resume) {
+        # We are resuming.  Denote that we've processed any genome that has a nonempty
+        # "non-peg-info" file. To do this, we need to do a recursive search through
+        # the GenomeData directory.
+        print "Scanning for existing genomes.\n";
+        my $genomeDir = $loader->genome_repo();
+        my $genomeHash = $loader->FindGenomeList($genomeDir, nameless => 1);
+        # Loop through the genomes in the directory, checking for "non-peg-info".
+        my $count = 0;
+        for my $genome (keys %$genomeHash) {
+            my $dirName = $genomeHash->{$genome};
+            if (-s "$dirName/non-peg-info") {
+                $genomesProcessed{$genome} = 1;
+                $stats->Add("genome-alreadyLoaded" => 1);
+                $count++;
+            }
         }
-        # Default the privilege to 0.
-        $priv //= $opt->privilege;
-        # Tell the helper (and the user) about this SEED.
-        print "Copying from level-$priv SEED at $figDisk.\n";
-        $loader->SetSEED($figDisk, $priv, \%genomesProcessed, $protFamRepo);
-        # Loop through the SEED commands.
-        my $done;
-        while (! $done) {
-            $line = <$ih>;
-            $stats->Add(subCommandLines => 1);
-            if (! defined $line || substr($line, 0, 1) eq '+') {
-                # Here we have a new section.
-                $done = 1;
-            } else {
-                $line =~ s/[\r\n]+$//;
-                if ($line eq '*Subsystems') {
-                    # Here the user wants to load all subsystems.  Get a list of the good ones.
-                    my $subList = $loader->ComputeSubsystems();
-                    # If we are resuming, delete any existing subsystems.
-                    if ($opt->resume) {
-                        print "Deleting old subsystem data.\n";
-                        File::Copy::Recursive::pathempty($loader->subsys_repo);
-                    }
-                    # Loop through the subsystems, loading them.
-                    print "Processing subsystems.\n";
-                    my ($count, $total) = (0, scalar @$subList);
-                    for my $sub (@$subList) {
-                        $count++;
-                        print "Loading subsystem $count of $total.\n";
-                        $loader->CopySubsystem($sub);
-                    }
-                } elsif ($line eq '*Genomes') {
-                    # Here the user wants to load all genomes.
-                    my $genomeList = $loader->AllGenomes();
-                    # Loop through the genomes, loading them.
-                    print "Processing genomes.\n";
-                    my($count, $total) = (0, scalar @$genomeList);
-                    for my $genome (@$genomeList) {
-                        $count++;
-                        print "Loading genome $count of $total.\n";
+        print "$count pre-existing genomes found.\n";
+    }
+    # Get the first line.
+    my $line = <$ih>;
+    # Loop through the input file.
+    while (defined $line) {
+        $line =~ s/[\r\n]+$//;
+        $stats->Add(commandLines => 1);
+        my ($command, @parms) = split /\t/, $line;
+        # Determine the purpose of this line.
+        if ($command eq '+SEED') {
+            # Here we have a new SEED.
+            my ($figDisk, $priv) = @parms;
+            # Insure we have a FIG disk.
+            if (! $figDisk) {
+                die "No FIG disk specified on +SEED command line.\n";
+            }
+            # Default the privilege to 0.
+            $priv //= $opt->privilege;
+            # Tell the helper (and the user) about this SEED.
+            print "Copying from level-$priv SEED at $figDisk.\n";
+            $loader->SetSEED($figDisk, $priv, \%genomesProcessed, $protFamRepo);
+            # Loop through the SEED commands.
+            my $done;
+            while (! $done) {
+                $line = <$ih>;
+                $stats->Add(subCommandLines => 1);
+                if (! defined $line || substr($line, 0, 1) eq '+') {
+                    # Here we have a new section.
+                    $done = 1;
+                } else {
+                    $line =~ s/[\r\n]+$//;
+                    if ($line eq '*Subsystems') {
+                        # Here the user wants to load all subsystems.  Get a list of the good ones.
+                        my $subList = $loader->ComputeSubsystems();
+                        # If we are resuming, delete any existing subsystems.
+                        if ($opt->resume) {
+                            print "Deleting old subsystem data.\n";
+                            File::Copy::Recursive::pathempty($loader->subsys_repo);
+                        }
+                        # Loop through the subsystems, loading them.
+                        print "Processing subsystems.\n";
+                        my ($count, $total) = (0, scalar @$subList);
+                        for my $sub (@$subList) {
+                            $count++;
+                            print "Loading subsystem $count of $total.\n";
+                            $loader->CopySubsystem($sub);
+                        }
+                    } elsif ($line eq '*Genomes') {
+                        # Here the user wants to load all genomes.
+                        my $genomeList = $loader->AllGenomes();
+                        # Loop through the genomes, loading them.
+                        print "Processing genomes.\n";
+                        my($count, $total) = (0, scalar @$genomeList);
+                        for my $genome (@$genomeList) {
+                            $count++;
+                            print "Loading genome $count of $total.\n";
+                            $loader->CopyGenome($genome);
+                        }
+                    } elsif ($line =~ /^(\d+\.\d+)/) {
+                        # Here the user wants to load a single genome.
+                        my $genome = $1;
+                        print "Loading single genome $genome.\n";
                         $loader->CopyGenome($genome);
+                    } else {
+                        # Here we have a subsystem name.
+                        print "Loading single subsystem $line.\n";
+                        my $dirName = $loader->CheckSubsystem($line);
+                        if ($dirName) {
+                            $loader->CopySubsystem($dirName);
+                        }
+                    }
+                }
+            }
+        } elsif ($command eq '+Taxonomy') {
+            # Here we have a request to download taxonomy data.
+            $loader->CopyTaxonomy($loader->taxRepo());
+            $line = <$ih>;
+        } elsif ($command eq '+Samples') {
+            # Here we have a sample directory.
+            my ($sampleDir) = @parms;
+            # Insure the directory is valid.
+            if (! $sampleDir) {
+                die "No directory specified for samples.";
+            } elsif (! -d $sampleDir) {
+                die "Invalid sample directory $sampleDir.";
+            } else {
+                # Upload the samples.
+                print "Sample directory $sampleDir selected.\n";
+                $loader->CopySamples($sampleDir, $loader->sampleRepo());
+            }
+            $line = <$ih>;
+        } elsif ($command eq '+PATRIC') {
+            # Get the privilege and the optional input file.
+            my ($priv, $file) = @parms;
+            if (! defined $priv || $priv eq '') {
+                $priv = $opt->privilege;
+            }
+            # Create the PATRIC helper.
+            print "Copying from level-$priv PATRIC.\n";
+            my $ploader = CopyFromPatric->new($priv, $opt, \%genomesProcessed, $protFamRepo);
+            # Determine how we're finding the genome IDs.  The default is to use the current file.
+            my $fh;
+            if ($file) {
+                print "Reading genome IDs from $file.\n";
+                open($fh, '<', $file) || die "Could not open genome ID input file: $!";
+            } else {
+                print "Reading genome IDs from input file.\n";
+                $fh = $ih;
+            }
+            my $done;
+            while (! $done) {
+                $line = <$fh>;
+                $stats->Add(patricGenomeLines => 1);
+                if (! defined $line || substr($line, 0, 1) eq '+') {
+                    # Here we have a new section.
+                    $done = 1;
+                    # If we read from a file, push forward to the next line.
+                    if ($file) {
+                        $line = <$ih>;
                     }
                 } elsif ($line =~ /^(\d+\.\d+)/) {
-                    # Here the user wants to load a single genome.
                     my $genome = $1;
-                    print "Loading single genome $genome.\n";
-                    $loader->CopyGenome($genome);
+                    # Copy the genome.
+                    $ploader->CopyGenome($genome);
                 } else {
-                    # Here we have a subsystem name.
-                    print "Loading single subsystem $line.\n";
-                    my $dirName = $loader->CheckSubsystem($line);
-                    if ($dirName) {
-                        $loader->CopySubsystem($dirName);
-                    }
+                    print "Invalid PATRIC input line: $line";
+                    $stats->Add(badInput => 1);
                 }
             }
-        }
-    } elsif ($command eq '+Taxonomy') {
-        # Here we have a request to download taxonomy data.
-        $loader->CopyTaxonomy($loader->taxRepo());
-        $line = <$ih>;
-    } elsif ($command eq '+Samples') {
-        # Here we have a sample directory.
-        my ($sampleDir) = @parms;
-        # Insure the directory is valid.
-        if (! $sampleDir) {
-            die "No directory specified for samples.";
-        } elsif (! -d $sampleDir) {
-            die "Invalid sample directory $sampleDir.";
-        } else {
-            # Upload the samples.
-            print "Sample directory $sampleDir selected.\n";
-            $loader->CopySamples($sampleDir, $loader->sampleRepo());
-        }
-        $line = <$ih>;
-    } elsif ($command eq '+PATRIC') {
-        # Get the privilege and the optional input file.
-        my ($priv, $file) = @parms;
-        if (! defined $priv || $priv eq '') {
-            $priv = $opt->privilege;
-        }
-        # Create the PATRIC helper.
-        print "Copying from level-$priv PATRIC.\n";
-        my $ploader = CopyFromPatric->new($priv, $opt, \%genomesProcessed, $protFamRepo);
-        # Determine how we're finding the genome IDs.  The default is to use the current file.
-        my $fh;
-        if ($file) {
-            print "Reading genome IDs from $file.\n";
-            open($fh, '<', $file) || die "Could not open genome ID input file: $!";
-        } else {
-            print "Reading genome IDs from input file.\n";
-            $fh = $ih;
-        }
-        my $done;
-        while (! $done) {
-            $line = <$fh>;
-            $stats->Add(patricGenomeLines => 1);
-            if (! defined $line || substr($line, 0, 1) eq '+') {
-                # Here we have a new section.
-                $done = 1;
-                # If we read from a file, push forward to the next line.
-                if ($file) {
-                    $line = <$ih>;
+            # Roll up the statistics.
+            $stats->Accumulate($ploader->stats);
+        } elsif ($command eq '+RAST') {
+            # Get the RAST directory and privilege level.
+            my ($rastDir, $priv) = @parms;
+            $priv //= $opt->privilege;
+            # Reset the loader in RAST mode.
+            $loader->Reset($priv, \%genomesProcessed, $protFamRepo, 1);
+            print "Copying from level-$priv RAST at $rastDir.\n";
+            $stats->Add(rastInstances => 1);
+            # Loop through the genomes.
+            my $done;
+            while (! $done) {
+                $line = <$ih>;
+                $stats->Add(subCommandLines => 1);
+                if (! defined $line || substr($line, 0, 1) eq '+') {
+                    # Here we have a new section.
+                    $done = 1;
+                } elsif ($line =~ /^(\d+\.\d+)\t(\d+)/) {
+                    my ($genome, $job) = ($1, $2);
+                    # Copy the genome.
+                    print "Copying RAST genome $genome.\n";
+                    $loader->CopyGenome($genome, "$rastDir/$job/rp/$genome");
+                } else {
+                    print "Invalid RAST input line: $line";
+                    $stats->Add(badInput => 1);
                 }
-            } elsif ($line =~ /^(\d+\.\d+)/) {
-                my $genome = $1;
-                # Copy the genome.
-                $ploader->CopyGenome($genome);
-            } else {
-                print "Invalid PATRIC input line: $line";
-                $stats->Add(badInput => 1);
             }
-        }
-        # Roll up the statistics.
-        $stats->Accumulate($ploader->stats);
-    } elsif ($command eq '+RAST') {
-        # Get the RAST directory and privilege level.
-        my ($rastDir, $priv) = @parms;
-        $priv //= $opt->privilege;
-        # Reset the loader in RAST mode.
-        $loader->Reset($priv, \%genomesProcessed, $protFamRepo, 1);
-        print "Copying from level-$priv RAST at $rastDir.\n";
-        $stats->Add(rastInstances => 1);
-        # Loop through the genomes.
-        my $done;
-        while (! $done) {
+        } else {
+            print "ERROR: Unknown command $command.\n";
+            $stats->Add(badCommands => 1);
             $line = <$ih>;
-            $stats->Add(subCommandLines => 1);
-            if (! defined $line || substr($line, 0, 1) eq '+') {
-                # Here we have a new section.
-                $done = 1;
-            } elsif ($line =~ /^(\d+\.\d+)\t(\d+)/) {
-                my ($genome, $job) = ($1, $2);
-                # Copy the genome.
-                print "Copying RAST genome $genome.\n";
-                $loader->CopyGenome($genome, "$rastDir/$job/rp/$genome");
-            } else {
-                print "Invalid RAST input line: $line";
-                $stats->Add(badInput => 1);
-            }
         }
-    } else {
-        print "ERROR: Unknown command $command.\n";
-        $stats->Add(badCommands => 1);
-        $line = <$ih>;
     }
+    print "End of command file.\n";
+    # Create the genome index.
+    $loader->IndexGenomes();
+    # Unspool the family data.
+    print "Fixing Protein Families.\n";
+    $protFamRepo->FixFunctions();
+    print "Unspooling Protein Families.\n";
+    $protFamRepo->output($opt->repo . '/Other');
+    # Compute the total time.
+    my $timer = time - $startTime;
+    $stats->Add(totalTime => $timer);
+};
+if ($@) {
+    print "Error occurred at " . localtime(time) . "\n";
 }
-print "End of command file.\n";
-# Create the genome index.
-$loader->IndexGenomes();
-# Unspool the family data.
-print "Fixing Protein Families.\n";
-$protFamRepo->FixFunctions();
-print "Unspooling Protein Families.\n";
-$protFamRepo->output($opt->repo . '/Other');
-# Compute the total time.
-my $timer = time - $startTime;
-$stats->Add(totalTime => $timer);
 # Tell the user we're done.
 print "All done.\n" . $stats->Show();
-
